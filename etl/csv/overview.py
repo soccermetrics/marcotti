bee083a0e7bdb50edc5c408aeac5b66cadd7b05f
Howard Hamilton <hhamilton@soccermetrics.net>
2016-02-05 02:27:28 -0500

    Add Club ingestion class, which was overlooked before

--- etl/csv/overview.py
+++ etl/csv/overview.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+from models.club import Clubs
 from models.common.overview import (DomesticCompetitions, InternationalCompetitions,
                                     Venues, VenueHistory, Surfaces, Countries, Timezones)
 from models.common.enums import ConfederationType
@@ -34,11 +35,34 @@ class CompetitionIngest(BaseCSV):
                 else:
                     print "Cannot insert Competition record: Neither Country nor Confederation defined"
                     continue
+                if len(insertion_list) == 50:
+                    self.session.add_all(insertion_list)
+                    self.session.commit()
+                    insertion_list = []
+        if len(insertion_list) != 0:
+            self.session.add_all(insertion_list)
 
-            if len(insertion_list) == 50:
-                self.session.add_all(insertion_list)
-                self.session.commit()
-                insertion_list = []
+
+class ClubIngest(BaseCSV):
+
+    def parse_file(self, rows):
+        insertion_list = []
+        for keys in rows:
+            name = self.column_unicode("Name", **keys)
+            country_name = self.column_unicode("Country", **keys)
+
+            if country_name is None:
+                print "Cannot insert Club record: Country required"
+                continue
+            else:
+                country_id = self.get_id(Countries, name=country_name)
+                club_dict = dict(name=name, country_id=country_id)
+                if country_id is not None and not self.record_exists(Clubs, **club_dict):
+                    insertion_list.append(Clubs(**club_dict))
+                    if len(insertion_list) == 50:
+                        self.session.add_all(insertion_list)
+                        self.session.commit()
+                        insertion_list = []
         if len(insertion_list) != 0:
             self.session.add_all(insertion_list)
 

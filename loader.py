06a8f5221e1b352151af576612dd0b52a5f7b7b7
Howard Hamilton <hhamilton@soccermetrics.net>
2016-02-10 22:58:25 -0500

    Make loader bypass missing data groups, add control blocks to set class parameters
    Allow for bypassing of groups that are missing or commented out of local config settings.
    Some data files include a third-party ID, so the ETL classes require the name of the data
    supplier as well.  Add the supplier to the class parameter tuple.

--- loader.py
+++ loader.py
@@ -8,16 +8,20 @@ if __name__ == "__main__":
     settings = LocalConfig()
     marcotti = Marcotti(settings)
     with marcotti.create_session() as sess:
-        for group in ['Overview', 'Personnel', 'Match']:
-            for entity, datafile in settings.CSV_DATA[group]:
-                if group in ['Overview', 'Personnel']:
+        for group in ['Supplier', 'Overview', 'Personnel', 'Match']:
+            for entity, datafile in settings.CSV_DATA.get(group, []):
+                if group in ['Supplier', 'Overview', 'Personnel']:
                     if entity == 'Venues':
                         params = (sess, settings.VENUE_EFF_DATE)
+                    elif entity in ['Competitions', 'Clubs', 'Players', 'Positions']:
+                        params = (sess, settings.DATA_SUPPLIER)
                     else:
                         params = (sess,)
                 else:
                     params = (sess, settings.COMPETITION_NAME, settings.SEASON_NAME)
-                if CSV_ETL_CLASSES[group][entity] is list:
+                    if entity == 'PlayerStats':
+                        params += (settings.DATA_SUPPLIER,)
+                if type(CSV_ETL_CLASSES[group][entity]) is list:
                     for etl_class in CSV_ETL_CLASSES[group][entity]:
                         ingest_feeds(get_local_handles, settings.CSV_DATA_DIR, datafile, etl_class(*params))
                 else:
